fd9fdba51692270ff2ca20357ef18b89d5a7c3ab
Adrien <72563434+adelestre@users.noreply.github.com>
2024-04-10 18:07:42 +0200

    Better specs navigation button for mobile (#3)

--- src/components/shared/core/guide/navigation/SelectSpecItem.tsx
+++ src/components/shared/core/guide/navigation/SelectSpecItem.tsx
@@ -1,6 +1,5 @@
 import { Option } from '@material-tailwind/react'
 import icons from '../../data/icons.json'
-import specs from '../../data/specs.json'
 import { useNavigate } from 'react-router-dom'
 import { useNavigationContext } from '../GuideContext'
 import { navigateToSpec } from '../../utilities/utils'
@@ -18,10 +17,6 @@ function SelectSpecItem({ job, spec }: Readonly<Props>) {
     icons[job as keyof typeof icons][
       spec as keyof (typeof icons)[keyof typeof icons]
     ]
-  const name: string =
-    specs[job as keyof typeof icons][
-      spec as keyof (typeof icons)[keyof typeof icons]
-    ]
   return (
     <Option
       value={job_spec}
@@ -30,11 +25,11 @@ function SelectSpecItem({ job, spec }: Readonly<Props>) {
       className="anim bg-accent-3 hover:bg-accent-4 border-accent-1 line-clamp-1 flex flex-none flex-row items-center gap-1 rounded-md border-2 px-2 py-1"
     >
       <img
-        className="h-6 w-6 rounded-sm border border-zinc-900"
+        className="h-7 w-7 rounded-sm border border-zinc-900"
         src={icon}
         alt={job_spec}
       />
-      <div className="font-bold text-zinc-900">{name}</div>
+      <div className="font-bold capitalize text-zinc-900">{spec}</div>
     </Option>
   )
 }
