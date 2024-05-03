b3950ac6f95dd2698f2c42a2a8f4cc475c783fed
Adrien Delestre <adrien.delestre@hotmail.com>
2024-05-03 20:40:50 +0200

    Remove MW Raid placeholders

--- src/components/specs/monk/mistweaver/raid/Raid.tsx
+++ src/components/specs/monk/mistweaver/raid/Raid.tsx
@@ -1,13 +1,9 @@
-import Spells from './spells/Spells'
-import Gameplay from './gameplay/Gameplay'
 import Build from './build/Build'
 
 function Raid() {
   return (
     <div>
       <Build />
-      {/* <Gameplay />
-      <Spells /> */}
     </div>
   )
 }
