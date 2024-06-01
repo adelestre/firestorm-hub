a03d551781d086fa018e26a14f6020bd5938ea46
adelestre <adrien.delestre@hotmail.com>
2024-06-02 00:18:38 +0200

    Fix typing issue that we don't care

--- src/components/shared/core/pages/leaderboard/api.ts
+++ src/components/shared/core/pages/leaderboard/api.ts
@@ -1,3 +1,3 @@
-export const apiBaseUrl = import.meta.env.PROD
+export const apiBaseUrl = (import.meta as any).env.PROD
   ? window.location.origin + '/api/leaderboard'
   : 'http://127.0.0.1:5001/firestorm-hub/us-central1/api/api/leaderboard'
